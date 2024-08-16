011083213434b5573976b7a4f09853aa111760c2
Harshiiet Negi <negeek7@gmail.com>
2024-08-16 19:29:49 +0530

    connecting to database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import products from './routes/products/index.js'
+import mongoose from 'mongoose';
 
 
 const app = express()
 const PORT = process.env.PORT
+const mongourl = process.env.MONGO_URL 
 
 
 // This is the main file
@@ -12,9 +14,18 @@ const PORT = process.env.PORT
 // Create a server
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
+    connectToDb()
 })
 
 // Initialize connection to mongodb via mongodb url
+async function connectToDb() {
+    try {
+        await mongoose.connect(mongourl + '/shop')
+        console.log("Connected to Database ✔")
+    } catch (error) {
+        console.log("Error connecting to database", error)
+    }
+}
 
 
 // Declare routes
