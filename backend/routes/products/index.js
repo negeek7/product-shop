508f066e1bca85cf411823841f0d8bdc57e1d55d
Harshiiet Negi <negeek7@gmail.com>
2024-08-17 13:47:06 +0530

    _id check

--- backend/routes/products/index.js
+++ backend/routes/products/index.js
@@ -10,11 +10,8 @@ router.get('/products', async (req, res) => {
     try {
         const products = await Product.find()
         console.log(products, "products")
-        if (products) {
-            res.status(200).json({ products, status: "Success" })
-        } else {
-            res.status(400).send("Something went wrong, try again!")
-        }
+        if (!products) return res.status(400).send("Something went wrong, try again!")
+        res.status(200).json({ products, status: "Success" })
     } catch (error) {
         res.status(400).send("Something went wrong, try again!")
         console.log("Error fetching products", error)
@@ -27,11 +24,12 @@ router.get('/product/:id', async (req, res) => {
     let productid = req.params.id
     if (!productid) return res.status(404).send("No product id found.")
     try {
-        const product = await Product.findOne({ id: productid })
+        const product = await Product.findOne({ _id: productid })
         if (!product) return res.status(400).json({ message: "Product not found" })
         res.status(200).json({ product, status: "Success" })
     } catch (error) {
         res.status(400).send("Something went wrong, try again!")
+        console.log("Error fetching products", error)
     }
 })
 
