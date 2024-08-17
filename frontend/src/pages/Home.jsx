52984e56d3ea391b6439019ba2d535df0dd70d5c
Harshiiet Negi <negeek7@gmail.com>
2024-08-18 02:57:05 +0530

    hadnling pagination frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,63 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import useFetch from '../hooks/useFetch'
 
 function Home() {
 
-  const [fetchError, setFetchError] = useState(false)
   const [productList, setProductList] = useState([])
+  const [pageNum, setPageNum] = useState(1)
+  const [totalPages, setTotalPages] = useState(null)
+  const [isFetchError, setIsFetchError] = useState(false)
+
+  console.log(productList)
+  console.log(totalPages)
+  console.log(pageNum)
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData(pageNum)
+  }, [pageNum])
 
   async function fetchData() {
-    const result = await fetch('http://localhost:8001/products')
-    if(!result.ok) return setFetchError(true)
-    else {
-      let list = await result.json()
-      setProductList(list)
+    try {
+      let result = await fetch(`http://localhost:8001/products?page=${pageNum}&pageSize=${6}`)
+      if (!result.ok) return setIsFetchError(true)
+      else {
+        result = await result.json()
+        setProductList(result.products)
+        setTotalPages(result.totalPages)
+      }
+    } catch (error) {
+      setIsFetchError(true)
     }
   }
 
-  console.log(productList, "PRODUCTS")
+  const handlePagination = (type) => {
+    setPageNum(prevPageNum => {
+      if (type === 'previous' && prevPageNum > 1) {
+        return prevPageNum - 1;
+      } else if (type === 'next' && prevPageNum < totalPages) {
+        return prevPageNum + 1;
+      }
+      return prevPageNum;
+    });
+  };
 
   return (
     <div>
-      Product Page
+
+
+      <button
+        onClick={() => handlePagination('previous')}
+      >
+        Previous
+      </button>
+
+      <button
+        onClick={() => handlePagination('next')}
+      >
+        Next
+      </button>
+
+
     </div>
   )
 }
