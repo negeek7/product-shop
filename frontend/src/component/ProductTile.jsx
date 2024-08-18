1a4a3279bd75f4d0263a63b20ecde74e2be4d6a5
Harshiiet Negi <negeek7@gmail.com>
2024-08-18 17:38:39 +0530

    product tile design

--- frontend/src/component/ProductTile.jsx
+++ frontend/src/component/ProductTile.jsx
@@ -3,37 +3,47 @@ import sampleimg from '../assets/pat-taylor-12V36G17IbQ-unsplash.jpg'
 
 function ProductTile({ product }) {
 
+  let { name, price, features, isBestSeller, images } = product
   const [addCount, setAddCount] = useState(0)
 
 
   return (
-    <div 
-      className="relative flex flex-col pb-2 rounded-md cursor-pointer border-none shadow-xl"
-      style={{width:"300px"}}
+    <div
+      className="relative flex flex-col pb-2 rounded-md cursor-pointer shadow-md border-none hover:scale-105
+      hover:shadow-blue-900 transition ease-in duration-300"
+      style={{ width: "300px" }}
     >
 
       {/* top segement */}
-      <div className="flex-1">
-        <div className="h-52 relative w-full rounded-md">
-          <img src={sampleimg} style={{height:"100%", width:"100%", objectFit: "cover"}} />
-        </div>
+      <div className="h-60 relative w-full">
+        <img src={sampleimg} className="rounded-md" style={{ height: "100%", width: "100%", objectFit: "cover" }} />
+      </div>
 
-        <div className="py-2 px-4">
-          <p>{product.name}</p>
+      <div className="flex-1 py-2 px-4 flex flex-col">
+        <div className="flex-1">
+          <p className="text-lg">{name}</p>
 
           {
-            product.features && product.features.map(feature => (
-              <p>{feature}</p>
-            ))
+            features &&
+            <div className="truncate w-full">
+              {
+                features.map((feature, i) => (
+                  <span className="text-white opacity-60 text-xs">{feature}{i === features.length - 1 ? '' : ','}&nbsp;</span>
+                ))
+              }
+            </div>
+          }
+          {
+            isBestSeller && <p className="bg-orange-400 mt-2 p-1 w-fit text-xs rounded-lg">Best selling product</p>
           }
 
-          <p>{product.price}</p>
         </div>
+        <p className="mt-4 flex-end">₹{price}</p>
       </div>
 
 
       {/* bottom segement */}
-      <button className="mx-2">Add to Cart</button>
+      <button className="mx-2 mt-2 float-end font-bold focus:outline-none hover:bg-gray-600 border-none">Add to Cart</button>
 
     </div>
   )
